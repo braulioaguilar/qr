0ef880e3c32f70ff90caae8daf856aab755afac7
Braulio Aguilar <braulio.inf@gmail.com>
2020-06-03 21:24:00 -0500

    add mux

--- src/main.go
+++ src/main.go
@@ -4,15 +4,19 @@ import (
 	"fmt"
 
 	"github.com/braulioinf/QRLauncher/src/app"
+	"github.com/gorilla/mux"
 )
 
 func main() {
 
-	app := app.App{}
+	app := app.App{
+		Router: mux.NewRouter().StrictSlash(true),
+	}
 
 	if err := app.Initialize(); err != nil {
 		fmt.Printf("MySQL error conexion. %v\n", err)
 	}
 
+	app.SetupRouter()
 	app.Run()
 }
